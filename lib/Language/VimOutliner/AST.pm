de74965cb82fb228ce67f4b96f0f47fa8ea80bba
Zakariyya Mughal <zaki.mughal@gmail.com>
2016-09-17 15:24:19 -0500

    init

---
+++ lib/Language/VimOutliner/AST.pm
@@ -0,0 +1,16 @@
+package Language::VimOutliner::AST;
+
+use Modern::Perl;
+
+1;
+=pod
+
+=head1 SYNOPSIS
+
+  use Language::VimOutliner::AST;
+
+=head1 DESCRIPTION
+
+A grammar for parsing Vim Outliner files.
+
+=cut
