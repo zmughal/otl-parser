103b585b5c6b4eb519d1830e1ae6584a1899f07b
Zakariyya Mughal <zaki.mughal@gmail.com>
2016-09-24 10:30:04 -0500

    Mark string as debug

--- lib/Language/VimOutliner/Shallow/Heading.pm
+++ lib/Language/VimOutliner/Shallow/Heading.pm
@@ -6,11 +6,15 @@ use Language::VimOutliner::Semantics;
 
 use overload
 	'""' => sub {
-		Language::VimOutliner::Semantics->classify($_[0]->text) . " : "
-		. $_[0]->text
-			=~ s/[A-Z]/%/gr
-			=~ s/[A-Za-z0-9]/x/gr
-			=~ s/%/X/gr
+		my $text = $_[0]->text;
+		#return $text;
+
+		#DEBUG
+		return
+			Language::VimOutliner::Semantics->classify($text) . " : "
+				. $text =~ s/[A-Z]/%/gr
+					=~ s/[A-Za-z0-9]/x/gr
+					=~ s/%/X/gr;
 	};
 
 has text => ( is => 'rw', isa => Str );
